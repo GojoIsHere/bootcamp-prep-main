ecdda6c588a00ca1b312a68d1b9c936f2fc85037
GojoIsHere <sus.thanet0726@gmail.com>
2023-06-02 11:39:09 +0545

    started control flow and completed founder-finder

--- pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
+++ pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -1,7 +1,14 @@
 let name = 'David';
 // let name = 'Nimit';
 // let name = 'Someone else';
+let found = true;
+let not_found = false;
 
-let found;
 
-// YOUR CODE BELOW
+if( name === 'David'){
+    console.log(found);
+}else if(name === 'Nimit'){
+    console.log(found);
+}else{
+    console.log(not_found);
+}
