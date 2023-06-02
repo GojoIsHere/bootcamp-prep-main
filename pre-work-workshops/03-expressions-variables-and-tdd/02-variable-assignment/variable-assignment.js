984f3b8acef1aad3d56624c073f1a0642d33b957
GojoIsHere <sus.thanet0726@gmail.com>
2023-06-02 11:21:24 +0545

    final update to expression and variable

--- pre-work-workshops/03-expressions-variables-and-tdd/02-variable-assignment/variable-assignment.js
+++ pre-work-workshops/03-expressions-variables-and-tdd/02-variable-assignment/variable-assignment.js
@@ -1,5 +1,5 @@
 let myFirstName = "Sushil";
-let myFavoriteNum = "8";
+let myFavoriteNum = 8;
 let havingFun = true;
 
 console.log(myFavoriteNum, "is",myFirstName,"'s", "lucky number and that is", havingFun );
--- pre-work-workshops/03-expressions-variables-and-tdd/05-take-the-shortcut/take-the-shortcut.js
+++ pre-work-workshops/03-expressions-variables-and-tdd/05-take-the-shortcut/take-the-shortcut.js
@@ -2,5 +2,10 @@ let happyNum = 1000;
 
 let newNum =2000;
 let result = newNum/=happyNum;
+result*=2;
+result+=1;
 
-console.log(result*2+1);
+console.log(result);
+happyNum=result;
+
+console.log(happyNum===result);
