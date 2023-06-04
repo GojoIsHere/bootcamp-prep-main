fb6df98eaa275133d8113b2c406ca66cac6ee678
GojoIsHere <sus.thanet0726@gmail.com>
2023-06-05 03:25:12 +0545

    correction of control flow task

--- pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
+++ pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
@@ -1,18 +1,13 @@
 let boolean1 = true, boolean2 = true;
-// let boolean1 = true, boolean2 = false;
-// let boolean1 = false, boolean2 = true;
-// let boolean1 = false, boolean2 = false;
-
 let result;
 
-if(boolean1=== true && boolean2 === true){
-    result = "truetrue";
-    let both = result;
-}else if( boolean1=== false || boolean2 === true){
-    result = "falsetrue";
-    let one = result;
+
+if(boolean1 && boolean2){
+    result="both";
+}
+else if(boolean1 || boolean2){
+    result="one"
 }
 else{
-    result = "falsefasle";
-    let none = result;
+    result="none"
 }
