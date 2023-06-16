9e7365a7033b968e972c0f53668aa4fcfe7d193e
GojoIsHere <sus.thanet0726@gmail.com>
2023-06-16 16:19:48 +0545

    update of (who's special) in High Order Function

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,13 +1,15 @@
 function whosASpecial(arrayOfPet){
-    let a =[];
-    let b ='';
-    for(let i = 0; i < arrayOfPet.length; i ++ ){
-        a.push(arrayOfPet[i].name+" the "+arrayOfPet[i].species+" is very special!");
-        b = a.join(" ");
-    }
-    ;
-    return b;
+  let a =[];
+  let b ='';
+  arrayOfPet.forEach(function(arrayOfPet){
+    a.push(arrayOfPet.name+" the "+arrayOfPet.species+" is very special!");
+    b = a.join(" ");
+    
+  })
+  return b;   
 }
+
+
 let specialPets = [{
     name: 'Sadie',
     species: 'cat'
