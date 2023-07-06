acccf4ef18f565666ce22a9bdc9fd15dae37b86e
GojoIsHere <sus.thanet0726@gmail.com>
2023-07-06 16:29:25 +0545

    updated of high order

--- workshops/11-higher-order-functions-ii/02-times-table/times-table.js
+++ workshops/11-higher-order-functions-ii/02-times-table/times-table.js
@@ -1,12 +1,16 @@
-function timesTable (anyNum){
-    function table(tableNum){
-        let result = anyNum* tableNum;
-        return result;
-    }
-    return table;
+// function timesTable (anyNum){
+//     function table(tableNum){
+//         let result = anyNum* tableNum;
+//         return result;
+//     }
+//     return table;
+// }
+
+// let a = timesTable(8);
+// let b = a(8);
+// console.log(b);
+function timesTable(num1) {
+  return function (num2) {
+    return num1 * num2;
+  };
 }
-
-let a = timesTable(8);
-let b = a(8);
-console.log(b);
-
--- workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,18 +1,33 @@
-function adds10(num) {
-    let start = Date.now();
-    let end = Date.now();
-  
-    // pause for 3 seconds, to make the function expensive!
-    while (end - start < 3000) {
-      end = Date.now();
+// function adds10(num) {
+//     let start = Date.now();
+//     let end = Date.now();
+
+//     // pause for 3 seconds, to make the function expensive!
+//     while (end - start < 3000) {
+//       end = Date.now();
+//     }
+
+//     return num + 10;
+//   }
+
+//   let cachedAdds10 = cacheSavings(adds10);
+
+//   cachedAdds10(20); // => returns 30, takes 3 seconds!
+//   cachedAdds10(0); // returns 10, takes 3 seconds!
+
+//   cachedAdds10(20); // => returns 30, takes no time at all!
+
+function cacheSavings(myCallback) {
+  let cacheVal = 0;
+  let cach;
+  return function myReturnFunc(arg) {
+    if (arg === cacheVal) {
+      //check if already present in cache
+      return cach;
+    } else {
+      cacheVal = arg;
+      cach = myCallback(arg); //invoke if not present
+      return cach;
     }
-  
-    return num + 10;
-  }
-  
-  let cachedAdds10 = cacheSavings(adds10);
-  
-  cachedAdds10(20); // => returns 30, takes 3 seconds!
-  cachedAdds10(0); // returns 10, takes 3 seconds!
-  
-  cachedAdds10(20); // => returns 30, takes no time at all!
+  };
+}
