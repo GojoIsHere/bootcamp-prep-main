918337fc8390cd8de5f5228f99c1a4f6b672eb3e
GojoIsHere <sus.thanet0726@gmail.com>
2023-07-19 07:34:53 +0545

    pbv-pbr complete

--- workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,12 +1,23 @@
-function veryOddMutant(arrayOfNum){
-    for(i in arrayOfNum){
-        if(arrayOfNum[i]%2 === 0){
-            arrayOfNum[i]= 'normie';
-        }
-    }
-    return arrayOfNum;
-}
-let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
-let countRemoved = veryOddMutant(allTheNums);
+// function veryOddMutant(arrayOfNum){
+//     for(i in arrayOfNum){
+//         if(arrayOfNum[i]%2 === 0){
+//             arrayOfNum[i]= 'normie';
+//         }
+//     }
+//     return arrayOfNum;
+// }
+// let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+// let countRemoved = veryOddMutant(allTheNums);
 
-console.log('allTheNums:', allTheNums);
+// console.log('allTheNums:', allTheNums);
+function veryOddMutant(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (!(arr[i] % 2)) {
+      arr[i] = "normie";
+      count++;
+    }
+  }
+  console.log(arr);
+  return count;
+}
--- workshops/09-pbv-pbr/04-my-splice/my-splice.js
+++ workshops/09-pbv-pbr/04-my-splice/my-splice.js
@@ -1,68 +1,76 @@
 debugger;
-function mySplice(arrayName, startIndx, numOfElement, replaceElement){
-   
-        if(arrayName !== undefined && startIndx === undefined && numOfElement === undefined && replaceElement === undefined)return arrayName;
-        else if(arrayName !== undefined && startIndx !== undefined && numOfElement !== undefined && replaceElement === undefined)
-        {
-            let newArray = [];
-            for(let i = 0; i<startIndx;i++){
-                newArray.push(arrayName[i]);
-                
-            }
-            if(arrayName.length>(startIndx+numOfElement)){
-                for(let i=(startIndx+numOfElement); i<arrayName.length;i++){
-                    newArray.push(arrayName[i]);
-
-                }
-                for(let i = arrayName.length-1; i>=0;i--){
-                    arrayName.pop(arrayName[i]);
-                }
-                for(let i in newArray){
-                    arrayName.push(newArray[i])
-                }
-                return arrayName;
-            }
-            for(let i = arrayName.length-1; i>=0;i--){
-                arrayName.pop(arrayName[i]);
-            }
-            for(let i in newArray){
-                arrayName.push(newArray[i])
-            }
-            return arrayName;
-        }
-        else if(arrayName !== undefined && startIndx !== undefined && numOfElement !== undefined && replaceElement !== undefined){
-            let newArray = [];
-            for(let i = 0; i<startIndx;i++){
-                newArray.push(arrayName[i]);
-                
-            }
-            newArray.push(replaceElement);
-            if(arrayName.length>(startIndx+numOfElement)){
-                for(let i=(startIndx+numOfElement); i<arrayName.length;i++){
-                    newArray.push(arrayName[i]);
-
-                }
-                for(let i = arrayName.length-1; i>=0;i--){
-                    arrayName.pop(arrayName[i]);
-                }
-                for(let i in newArray){
-                    arrayName.push(newArray[i])
-                }
-                return arrayName;
-            }
-            for(let i = arrayName.length-1; i>=0;i--){
-                arrayName.pop(arrayName[i]);
-            }
-            for(let i in newArray){
-                arrayName.push(newArray[i])
-            }
-            return arrayName;
-        }else{
-            console.log('not applicable');
-        }
-        
-    
+function mySplice(arrayName, startIndx, numOfElement, replaceElement) {
+  if (
+    arrayName !== undefined &&
+    startIndx === undefined &&
+    numOfElement === undefined &&
+    replaceElement === undefined
+  )
+    return arrayName;
+  else if (
+    arrayName !== undefined &&
+    startIndx !== undefined &&
+    numOfElement !== undefined &&
+    replaceElement === undefined
+  ) {
+    let newArray = [];
+    for (let i = 0; i < startIndx; i++) {
+      newArray.push(arrayName[i]);
+    }
+    if (arrayName.length > startIndx + numOfElement) {
+      for (let i = startIndx + numOfElement; i < arrayName.length; i++) {
+        newArray.push(arrayName[i]);
+      }
+      for (let i = arrayName.length - 1; i >= 0; i--) {
+        arrayName.pop(arrayName[i]);
+      }
+      for (let i in newArray) {
+        arrayName.push(newArray[i]);
+      }
+      return arrayName;
+    }
+    for (let i = arrayName.length - 1; i >= 0; i--) {
+      arrayName.pop(arrayName[i]);
+    }
+    for (let i in newArray) {
+      arrayName.push(newArray[i]);
+    }
+    return arrayName;
+  } else if (
+    arrayName !== undefined &&
+    startIndx !== undefined &&
+    numOfElement !== undefined &&
+    replaceElement !== undefined
+  ) {
+    let newArray = [];
+    for (let i = 0; i < startIndx; i++) {
+      newArray.push(arrayName[i]);
+    }
+    newArray.push(replaceElement);
+    if (arrayName.length > startIndx + numOfElement) {
+      for (let i = startIndx + numOfElement; i < arrayName.length; i++) {
+        newArray.push(arrayName[i]);
+      }
+      for (let i = arrayName.length - 1; i >= 0; i--) {
+        arrayName.pop(arrayName[i]);
+      }
+      for (let i in newArray) {
+        arrayName.push(newArray[i]);
+      }
+      return arrayName;
+    }
+    for (let i = arrayName.length - 1; i >= 0; i--) {
+      arrayName.pop(arrayName[i]);
+    }
+    for (let i in newArray) {
+      arrayName.push(newArray[i]);
+    }
+    return arrayName;
+  } else {
+    console.log("not applicable");
+  }
 }
+
 // let myArray = [1, 2, 3];
 
 // let a = mySplice(myArray, 1, 1, 'apples') // => [2]
@@ -72,8 +80,34 @@ function mySplice(arrayName, startIndx, numOfElement, replaceElement){
 // b = mySplice(funNums, 2, 3);
 // console.log(b);
 
-let ourStuff = ['food', 'trash', 'clothes'];
+// let ourStuff = ['food', 'trash', 'clothes'];
 
-c =  mySplice(ourStuff, 1, 1);
-console.log(c);
-console.log(ourStuff);
+// c =  mySplice(ourStuff, 1, 1);
+// console.log(c);
+// console.log(ourStuff);
+
+function mySplice(arr, startIdx, rmvNo, addElement) {
+  let newArray = [];
+  let deleteElements = [];
+  for (let k = 0; k < startIdx; k++) {
+    newArray.push(arr[k]);
+  }
+  if (addElement !== undefined) {
+    newArray.push(addElement);
+  }
+  for (let i = startIdx; i < startIdx + rmvNo; i++) {
+    deleteElements.push(arr[i]);
+  }
+  for (let j = startIdx + rmvNo; j < arr.length; j++) {
+    newArray.push(arr[j]);
+  }
+  for (let k = 0; k < arr.length; k++) {
+    arr.pop();
+  }
+  for (let i = 0; i < newArray.length; i++) {
+    arr[i] = newArray[i];
+  }
+  // console.log(deleteElements)
+  // console.log(newArray)
+  return deleteElements;
+}
